a765ed19305ab7f0c280e3d3e777adf008a0d612
Ekaterina <sweetykiti@gmail.com>
2022-12-28 20:08:31 +0300

    Решение третьей задачи

---
+++ Task04/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+void PrintArr3D(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($" {array[i, j, k]}  [{i}, {j} ,{k}] ");
+            }
+
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,,] FillArray3D(int x, int y, int z , int min, int max)
+{
+    int[,,] array = new int[x, y, z];
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            for (int k = 0; k < z; k++)
+            {
+                array[i, j, k] = new Random().Next(min, max);
+            }
+
+        }
+    }
+    return array;
+}
+
+int[, ,] array1 = FillArray3D(2, 2, 2, 10, 99);
+PrintArr3D(array1);
