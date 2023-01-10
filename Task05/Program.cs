b35cce88ce0662b79d3f1bb23ac90f47d5a111e9
Ekaterina <sweetykiti@gmail.com>
2023-01-10 21:57:16 +0300

    Решение пятой задачи

---
+++ Task05/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+void PrintArray(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if(array[i, j]< 10){Console.Write($"0{array[i, j]} ");}
+            else{Console.Write($"{array[i, j]} ");}
+        }
+        Console.WriteLine("");
+    }
+}
+int [,] AddArrayRound(int [,] array)
+{
+    int n = array.GetLength(0);
+    int m = array.GetLength(1);
+    int count = 1;
+    for (int i = 0, j = 0, k = 0; n != 0; i++,j++)
+    {
+        for (k = 0; k < m - 1; k++) {array[i, j++] = count++;}
+        for (k = 0; k < n - 1; k++) {array[i++, j] = count++;}
+        for (k = 0; k < m - 1; k++) {array[i, j--] = count++;}
+        for (k = 0; k < n - 1; k++) {array[i--, j] = count++;}
+        n = n < 2 ? 0 : n - 2;
+        m = m < 2 ? 0 : m - 2;
+    }
+    return array;
+}
+
+int [,] array = new int [4,4];
+array = AddArrayRound(array);
+PrintArray(array);
